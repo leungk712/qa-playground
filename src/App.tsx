301163138e114cc9165de865db6c28993a5e4623
Kevin Leung <kevin.leung@nagrastar.com>
2024-07-25 12:54:17 -0600

    feat: initialize app

---
+++ src/App.tsx
@@ -0,0 +1,215 @@
+import { useState } from 'react';
+
+// ===== Material UI ===== //
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import {
+  AppBar as MuiAppBar,
+  AppBarProps as MuiAppBarProps,
+  Box,
+  Divider,
+  Drawer as MuiDrawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  Toolbar,
+} from '@mui/material';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import InboxIcon from '@mui/icons-material/Inbox';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+// ===== Components ===== //
+import Login from './pages/Login';
+
+// ===== Interfaces ===== //
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+// ===== React Router ===== //
+import { Routes, Route, useLocation } from 'react-router-dom';
+
+// ===== Styles ===== //
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(6),
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: '50px',
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: `${drawerWidth}px`,
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
+  ...(open && {
+    ...openedMixin(theme),
+    '& .MuiDrawer-paper': openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    '& .MuiDrawer-paper': closedMixin(theme),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+function App() {
+  const theme = useTheme();
+  const location = useLocation();
+
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleDrawer = (status: boolean) => {
+    setOpen(status);
+  };
+
+  return (
+    <>
+      <Box sx={{ display: 'flex' }}>
+        <AppBar
+          position="fixed"
+          sx={{
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+            bgcolor: '#00897b',
+          }}
+          open={open}
+        >
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              aria-label="menu"
+              sx={{
+                mr: 2,
+                color: 'white',
+                ...(open && { display: 'none' }),
+              }}
+              onClick={() => handleDrawer(!open)}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Typography variant="h6" component="div">
+              QA Playground
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open} sx={{ width: drawerWidth }}>
+          <DrawerHeader sx={{ bgcolor: '#00897b' }}>
+            <IconButton onClick={() => handleDrawer(false)}>
+              {theme.direction === 'rtl' ? (
+                <ChevronRightIcon sx={{ color: 'white' }} />
+              ) : (
+                <ChevronLeftIcon sx={{ color: 'white' }} />
+              )}
+            </IconButton>
+          </DrawerHeader>
+
+          <Divider />
+
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+      </Box>
+
+      <Main open={open} sx={{ height: '100vh' }}>
+        <Routes key={location.pathname} location={location}>
+          <Route index={true} element={<Login />} />
+        </Routes>
+      </Main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,17 @@
+// ===== Material UI ===== //
+
+// ===== Components ===== //
+
+// ===== Constants ===== //
+
+// ===== Helpers ===== //
+
+// ===== Interfaces ===== //
+
+// ===== Redux ===== //
+
+// ===== Styles ===== //
+
+export default function Login() {
+  return <h1>Login!</h1>;
+}
